2bec4cf634aad2846681f9099f31c9c6768f7aa6
Joanna Kolakowska <kolakowska.joanna.jk@gmail.com>
2023-03-11 13:02:23 +0100

    niby dziala

--- index.js
+++ index.js
@@ -4,23 +4,30 @@ const currencyButton = document.querySelector("#currencyButton");
 const comeout = document.querySelector("#comeout");
 
 const chooseCurrency = async (event) => {
+  try {
     let chosenCurrencyRate;
-    const response = await fetch(`http://api.nbp.pl/api/exchangerates/rates/a/${event.target.value}/?format=json`)
+    const response = await fetch(
+      `http://api.nbp.pl/api/exchangerates/rates/a/${event.target.value}/?format=json`
+    );
     const finalResponse = await response.json();
-    chosenCurrencyRate = await finalResponse.rates[0].mid;
+    chosenCurrencyRate = finalResponse.rates[0].mid;
     console.log(chosenCurrencyRate);
-    return newFunction(chosenCurrencyRate);
-};
-const newFunction = (parametr) => {
-    const currencyRate = parametr;
-    return currencyRate;
-};
-console.log(currencyRate);
-const getComeout = () => {
-     const finalValue = (valueInput.value * currencyRate).toFixed(2)
-     comeout.innerHTML = finalValue;
+    const getComeout = () => {
+        const finalValue = (valueInput.value * chosenCurrencyRate).toFixed(2);
+        comeout.innerHTML = finalValue;
+      };
+      currencyButton.addEventListener("click", getComeout);
+ //   return chosenCurrencyRate;
+  } catch (err) {
+    console.log(err);
+  }
 };
 
+//console.log(chosenCurrencyRate);
+/*const getComeout = () => {
+  const finalValue = (valueInput.value * chosenCurrencyRate).toFixed(2);
+  comeout.innerHTML = finalValue;
+};*/
+
 currencySelector.addEventListener("change", chooseCurrency);
-currencyButton.addEventListener("click", getComeout);
-console.log(currencyRate);
+//currencyButton.addEventListener("click", getComeout);
