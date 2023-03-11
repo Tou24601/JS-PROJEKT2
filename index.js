dbd8275081fc7c7379bf706ba4f759cf9a63988e
Joanna Kolakowska <kolakowska.joanna.jk@gmail.com>
2023-03-11 18:23:40 +0100

    done

--- index.js
+++ index.js
@@ -5,29 +5,20 @@ const comeout = document.querySelector("#comeout");
 
 const chooseCurrency = async (event) => {
   try {
-    let chosenCurrencyRate;
     const response = await fetch(
       `http://api.nbp.pl/api/exchangerates/rates/a/${event.target.value}/?format=json`
     );
     const finalResponse = await response.json();
-    chosenCurrencyRate = finalResponse.rates[0].mid;
-    console.log(chosenCurrencyRate);
-    const getComeout = () => {
-        const finalValue = (valueInput.value * chosenCurrencyRate).toFixed(2);
-        comeout.innerHTML = finalValue;
-      };
-      currencyButton.addEventListener("click", getComeout);
- //   return chosenCurrencyRate;
+    const chosenCurrencyRate = await finalResponse.rates[0].mid;
+    currencyButton.addEventListener("click", () => getComeout(chosenCurrencyRate));
   } catch (err) {
     console.log(err);
   }
 };
 
-//console.log(chosenCurrencyRate);
-/*const getComeout = () => {
+const getComeout = (chosenCurrencyRate) => {
   const finalValue = (valueInput.value * chosenCurrencyRate).toFixed(2);
   comeout.innerHTML = finalValue;
-};*/
+};
 
 currencySelector.addEventListener("change", chooseCurrency);
-//currencyButton.addEventListener("click", getComeout);
